e13c4847e92cf049db3649fbe486794907765db3
gaga <techang2009@126.com>
2016-10-09 13:29:11 +0800

    update

--- start.py
+++ start.py
@@ -2,15 +2,15 @@ from flask import Flask
 from flask_restful import Resource, Api
 from flask.ext.mysql import MySQL
 from flask_restful import reqparse
-
+import datetime
 mysql = MySQL()
 app = Flask(__name__)
 
-# MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'zhulong'
+# MySQL configurationsU
+app.config['MYSQL_DATABASE_USER'] = 'daitechang'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'zhulong123'
 app.config['MYSQL_DATABASE_DB'] = 'radius'
-app.config['MYSQL_DATABASE_HOST'] = '47.88.149.225'
+app.config['MYSQL_DATABASE_HOST'] = '42.96.152.105'
 
 
 mysql.init_app(app)
@@ -31,7 +31,6 @@ class CreateUser(Resource):
 			userId = args['userid']
 			if userId is None:
 				return {'error': 'no userid'} 
-
 			conn = mysql.connect()
 			cursor = conn.cursor()
 			sql = "select * from radcheck where username = '%s'" % str(userId)
@@ -39,14 +38,14 @@ class CreateUser(Resource):
 			data = cursor.fetchall()
 			if len(data) == 0:
 
-				sql = "insert into radcheck (username, attribute, op, value) VALUES ('%s', 'Cleartext-Password', ':=', 'gagatechang') " % userId 
+				sql = "insert into radcheck (username, attribute, op, value, dsttime) VALUES ('%s', 'Cleartext-Password', ':=', 'gagatechang', '%s') " % (userId, (datetime.datetime.now() + datetime.timedelta(days=31)).strftime("%Y-%m-%d %H:%M")) 
 				print sql
 				cursor.execute(sql)
 				conn.commit()
 			return {'statusCode':200}
 
 		except Exception as e:
-		
+			print str(e)		
 			return {'error': str(e)}
 
 class GetUser(Resource):
@@ -54,17 +53,26 @@ class GetUser(Resource):
 		try:
 			conn = mysql.connect()
 			cursor = conn.cursor()
-			sql = "select * from radpostauth"
+			parser = reqparse.RequestParser()
+			parser.add_argument('userid', type=str)
+			args = parser.parse_args()
+			userId = args['userid']
+			if userId is None:
+				return {'error': 'no userid'}
+			sql = "select * from radcheck where username = '%s'" % str(userId)
 			cursor.execute(sql)
 			data = cursor.fetchall()
-
-			if len(data)>0 :
-				return {'statusCode':200,'UserId':str(data[0][0])}
+			ret = {}
+			if len(data) > 0:
+				for r in data:
+					ret['username'] = r[0]
+					ret['dsttime'] = r[5].strftime("%Y-%m-%d %H:%M")
+                        return {'statusCode':200, 'data':ret}
 		except Exception as e:
 			return {'error': str(e)}
 
 class GetServer(Resource):
-	def get(self):
+	def post(self):
 		try:
 			conn = mysql.connect()
 			cursor = conn.cursor()
@@ -72,6 +80,7 @@ class GetServer(Resource):
 			cursor.execute(sql)
 			data = cursor.fetchall()
 			ret = []
+			print 11111111
 			if len(data) > 0:
 				for r in data:
 					tmp = {} 
@@ -80,8 +89,28 @@ class GetServer(Resource):
 					tmp['name'] = r[2]
 					tmp['source'] = r[3]
 					ret.append(tmp)
-			return {'statusCode':200, 'data':ret}
+			parser = reqparse.RequestParser()
+			parser.add_argument('userid', type=str)
+			args = parser.parse_args()
+			userId = args['userid']
+			print userId
+			if userId is None:
+				return {'error': 'no userid'}
+			sql = "select * from radcheck where username = '%s'" % str(userId)
+			cursor.execute(sql)
+			data = cursor.fetchall()
+			user = {}
+			if len(data) > 0:
+				for r in data:
+					user['username'] = r[0]
+					user['dsttime'] = r[5].strftime("%Y-%m-%d %H:%M")
+
+
+			server = {'server':ret, 'user':user}
+			
+			return {'statusCode':200, 'data':server}
 		except Exception as e:
+			print str(e)
 			return {'error': str(e)}
 
 		
